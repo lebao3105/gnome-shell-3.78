b2ef80a863cbbb002f415c302f41c9391adabd03
Le Bao Nguyen <bao12345yocoo@gmail.com>
2022-08-19 17:49:03 +0700

    Nothing

--- update-theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#mkdir ~/.themes
-#kdir ~/.themes/JK
-#rm -rf build
-#meson build
-#ninja -C build data/theme/gnome-shell.css
-cp build/data/theme/gnome-shell.css ~/.themes/JK/gnome-shell
-echo "Done, please select JK shell theme from GNOME Tweaks."
